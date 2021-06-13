1d6e75e83c3ad59a47b90b890ea77ead714beee5
boulei_n <boulei_n@etna-alternance.net>
2021-06-13 17:38:56 +0200

    [develop] fix issue ai server

--- ai-server/app.js
+++ ai-server/app.js
@@ -1,11 +1,23 @@
-// Includes 
+// We set up the includes
 require('dotenv').config();
+const mysql = require('mysql');
+const PythonShell = require('python-shell').PythonShell;
 const tmi = require('tmi.js');
 const colors = require('colors/safe');
 const express = require('express');
-const spawn = require("child_process").spawn;
+const spawn = require('child_process').spawn;
 const app = express();
-// Datas
+const { count } = require('console');
+const { exit } = require('process');
+// We instance the logs
+const logger = require('./config/logger.js');
+logger.log('info', 'Script launched', { message: new Date + ' ------------------------------------------ ' });
+/*
+* Exemple :
+* logger.log('error', 'hello', { message: 'world' });
+*/
+
+// We instance the Datas 
 var tab = new Array();
 var t1;
 var t2;
@@ -14,9 +26,31 @@ var nbMessage1;
 var nbMessage2;
 var nbMessageSec;
 var nbMessageSec1 = 0;
-// Port
+// We set up the port
 const port = process.env.port || 8080;
-// Bot creation
+// We set up the database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'aicutdb_development'
+  });
+// We connect the database
+connection.connect((err) => {
+    if (err) {
+        logger.log('error', 'Error connecting to Db ', { message: err });
+        exit();
+    };
+    console.log(colors.bgRed.white('DB Connected!'));
+});  
+  
+// We get the first argument
+var Username = process.argv.slice(2);
+Username = Username[0];
+console.log(colors.bgGreen.magenta('This is the Username argument -> '+ Username));
+logger.log('info', 'Arguments', { message: 'UserId receipt :' + Username });
+
+// Bot message creation
 const client = new tmi.Client({
     options: {
         debug: true,
@@ -32,7 +66,7 @@ const client = new tmi.Client({
         password: process.env.TWITCH_PASSWORD
     },
     // Live stream to inspect
-    channels: [process.env.TWITCH_CHANNEL]
+    channels: [Username]
 });
 // We connect the bot
 client.connect().catch(console.error);
@@ -42,10 +76,10 @@ client.on('message', (channel, tags, message, self) => {
     if (self) return;
     t1 = Date.now()
     console.log(colors.bgCyan.red(message));
-	console.log(colors.bgCyan.blue(tags));
+	console.log(colors.bgCyan.red(tags));
 	// We add the message to the an array
     tab.push(message);
-	console.log(colors.bgCyan.green(tab.length));
+	console.log("The number of messages ->" + colors.bgCyan.green(tab.length));
 	// We set the Messages variables
     nbMessage1 = tab.length;
     nbMessage2 = tab.length;
@@ -75,25 +109,27 @@ client.on('message', (channel, tags, message, self) => {
 		console.log(colors.bgMagenta.blue(nbMessage));
 		console.log(colors.bgMagenta.blue(nbMessageSec1));
 
-        const PythonShell = require('python-shell').PythonShell;
-
         var options = {
             mode: 'text',
-            pythonPath: 'C:\Users\willi\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7',
             pythonOptions: ['-u'],
             scriptPath: './',
-            args: ['449300', '4000', '6000', '60', nbMessageSec, 'happy', nbMessageSec1, '9000', t2]
+            args: ['2554','532','874']
         };
 
         PythonShell.run('algorithme.py', options, function(err, results) {
-            if (err)
-                throw err;
-            // Results is an array consisting of messages collected during execution
-            console.log('results: %j', results);
+            if (err){
+                logger.log('error', 'Error executing the ML Script ', { message: err });
+            };
+            // If the machine learning detect smthing
+            if(results[0] == "True"){
+                console.log(colors.bgRed.white('Strong moment detected, Cut launched'));
+            }
+            else{
+                console.log(colors.bgRed.white('No strong moment detected'));
+            }
         });
 
     }
-    //app.listen(port, () => console.log(`Example app listening on port ${port}!`))
     t2 = t1;
     nbMessage2 = nbMessage1;
 });
