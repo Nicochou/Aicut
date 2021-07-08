73e1398a940fbfa78026e597b27f1abb8fa2dbcc
boulei_n <boulei_n@etna-alternance.net>
2021-07-08 10:16:00 +0200

    [main] ajout logs

--- ai-server/config/logger.js
+++ ai-server/config/logger.js
@@ -1,27 +1,17 @@
-const winston = require('winston');
- 
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
+const {
+  createLogger,
+  transports,
+  format
+} = require('winston');
+
+const logger = createLogger({
   transports: [
-    //
-    // - Write all logs with level `error` and below to `error.log`
-    // - Write all logs with level `info` and below to `combined.log`
-    //
-    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'logs/combined.log' }),
-  ],
-});
- 
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }));
-}
+      new transports.File({
+          filename:'logs/info.log',
+          level: 'info',
+          format: format.combine(format.timestamp(), format.json())
+      })
+  ]
+})
 
 module.exports = logger;
