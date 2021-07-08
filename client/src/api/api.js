fbf906d4c243f9058264b01c311a5f01645c9678
boulei_n <boulei_n@etna-alternance.net>
2021-07-08 18:55:53 +0200

    [develop] token client app

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -17,6 +17,7 @@ if(!localStorage.getItem('appToken')){
         grant_type : client_credentials
     })
     .then(response => {
+      console.log(response);
       // Si on une réponse on enregistre en session notre app Token  
       if (response.data) {
         localStorage.setItem("appToken", JSON.stringify(response.data));
@@ -28,9 +29,39 @@ if(!localStorage.getItem('appToken')){
 }
 // Si on a une session
 else{
-    // On charche notre variable depuis notre session
+    // On charge notre variable depuis notre session
     let appToken = JSON.parse(localStorage.getItem('appToken'));
     access_app_token = appToken.access_token
+    // On revoke le token 
+    axios
+    .post(config.TWITCH_APP_REVOKE_TOKEN, {
+        client_id : client_id,
+        token : access_app_token
+    })
+    .then(response => {
+      // Si on une réponse 200 au revoke du app token
+      if (response.status == 200) {
+          axios
+          .post(config.TWITCH_APP_URL_TOKEN, {
+              client_id : client_id,
+              client_secret : client_secret,
+              grant_type : client_credentials
+          })
+          .then(response => {
+            // Si on une réponse on enregistre en session notre app Token  
+            if (response.data) {
+              localStorage.setItem("appToken", JSON.stringify(response.data));
+              access_app_token =  response.data.access_token;
+            }
+          })
+          .catch(error => {
+              console.error(error.message);
+            });
+      }
+    })
+    .catch(error => {
+        console.error(error.message);
+      });   
 }
 
 // On creer les headers pour les requetes communes
