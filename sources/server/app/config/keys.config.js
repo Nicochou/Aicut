3440e94066c6463aca7fe896c05fcc16574feb50
boulei_n <boulei_n@etna-alternance.net>
2021-09-26 16:30:23 +0200

    [10] ajout readme

--- sources/server/app/config/keys.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	twitch: {
-		clientID: "rd3cibcx7qznkg2ajouvjm3902pslc",
-		clientSecret: "cu378g0j5uejc414yn0k9y72tphd6e",
-		callbackURL: "http://localhost:8082/twitchAuth/callback"
-	}
-}
--- sources/server/start.js
+++
@@ -1,6 +0,0 @@
-const app = require("./server.js");
-// set port, listen for requests
-const PORT = process.env.PORT || 8082;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
