65dac167b21e719420528dedd9e2ffaf3ed73136
NoRaged <NoRaged@william.broways@gmail.com>
2021-09-28 16:47:39 +0200

    changement de tout les lorem

--- client/src/components/app/mount.component.js
+++ client/src/components/app/mount.component.js
@@ -46,8 +46,7 @@ export default class Mount extends Component {
               <h5>Ajouter une intro</h5>
               {/* A modifier pour explication d'upload d'intro */}
               <p>
-                Qui anim magna cillum sint. Et fugiat velit ut id in non irure ea ex sunt aute. 
-                Irure fugiat dolore ea commodo sunt exercitation officia do voluptate eu do velit magna.
+                selectionné une video dans vos document afin qu'elle sois utilisé en tant qu'intro du best-of.
               </p>
               {/* A modifier pour upload vers bdd */}
               <iframe width="300" height="200" src="https://www.youtube.com/embed/watch?v=M2FSuCezaXs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
@@ -60,8 +59,7 @@ export default class Mount extends Component {
               <h5>Ajouter une outro</h5>
               {/* A modifier pour explication d'upload d'outro */}
               <p>
-                Qui anim magna cillum sint. Et fugiat velit ut id in non irure ea ex sunt aute. 
-                Irure fugiat dolore ea commodo sunt exercitation officia do voluptate eu do velit magna.
+              selectionné une video dans vos document afin qu'elle sois utilisé en tant qu'outro du best-of.
               </p>
               {/* A modifier pour upload vers bdd */}
               <iframe width="300" height="200" src="https://www.youtube.com/embed/watch?v=ntorJi-36ds" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
@@ -74,8 +72,7 @@ export default class Mount extends Component {
               <h5>Ajouter une transition</h5>
               {/* A modifier pour explication d'upload de transition */}
               <p>
-                Qui anim magna cillum sint. Et fugiat velit ut id in non irure ea ex sunt aute. 
-                Irure fugiat dolore ea commodo sunt exercitation officia do voluptate eu do velit magna.
+                selectionné une video dans vos document afin qu'elle sois utilisé en tant que transition entre chaque clip sélectionné du best-of.
               </p>
               {/* A modifier pour upload vers bdd */}
               <iframe width="300" height="200" src="https://www.youtube.com/embed/watch?v=GsZkdqt92NE&list=PLqt9Y2cXfjfhHOKKJ6LpC2x1EkXXPEH17" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>   
@@ -90,28 +87,15 @@ export default class Mount extends Component {
             <h3 class="col-sm">Veuillez lancer votre live stream</h3>
             {/* A modifier pour explication de lancer stream avant création clip */}
             <p class="col-sm">
-            Velit reprehenderit dolor irure pariatur. Aliquip do occaecat non
-            sint. Id ipsum sit reprehenderit amet. Ipsum mollit ut sint nostrud
-            do aliquip. Laboris tempor do minim ullamco. Magna et laborum ut
-            aliqua consectetur mollit ullamco aliqua laborum mollit duis.
+            Veuillez lancer votre stream afin de pouvoir demarrer la création de clip automatique.
             </p>
           </div>
         )}
-        <div class="row" id="tuto-clip">
+        <div class="row center" id="tuto-clip">
           <h4>Comment faire un montage ?</h4>
            {/* A modifier pour explication de création d'un clip */}
           <p class="col-sm">
-            Velit reprehenderit dolor irure pariatur. Aliquip do occaecat non
-            sint. Id ipsum sit reprehenderit amet. Ipsum mollit ut sint nostrud
-            do aliquip. Laboris tempor do minim ullamco. Magna et laborum ut
-            aliqua consectetur mollit ullamco aliqua laborum mollit duis.
-          </p>
-          {/* A modifier pour explication de création d'un clip */}
-          <p class="col-sm">
-            Velit reprehenderit dolor irure pariatur. Aliquip do occaecat non
-            sint. Id ipsum sit reprehenderit amet. Ipsum mollit ut sint nostrud
-            do aliquip. Laboris tempor do minim ullamco. Magna et laborum ut
-            aliqua consectetur mollit ullamco aliqua laborum mollit duis.
+          Tout d'abord vous devez lancer votre stream ensuite sur cette même page vous pourrez ajouter une intro, une transition et une outro ensuite vous pourrez sélectionné vos clip dans l'odre et cliquez sur le boutons monter ce qui vous créeras votre un best-of automatiquement que vous pourrez telecharger sur votre appareil.
           </p>
           {/* Source de la vidéo à modifier */}
           <iframe width="560" height="315" src="https://www.youtube.com/embed/Ak0sC_NpkKM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>      
